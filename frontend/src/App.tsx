e43440884f571e517bb8f5ec211a573034062abd
Fabio Leandro <fabiolpsolucoes@gmail.com>
2022-05-10 16:44:30 -0300

    Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,24 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
 import Navbar from './componentes/Navbar';
 
 function App() {
-  return(
-    <Navbar/>
-  )
-
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
